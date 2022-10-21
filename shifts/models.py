2ed88a99f6e8f2b339f782600889b8c7e094a9ce
k-roozbahani <kavehrozbah@gmail.com>
2022-10-21 16:43:33 +0330

    definde models to shifts model

--- shifts/models.py
+++ shifts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django_jalali.db import models as jmodels
 
 
+class ProgramMonth():
+    pass
+
+
+class DayShift(models.Model):
+    pass
+
+
+class NightShift(models.Model):
+    pass
+
+
+class ExpertShift(models.Model):
+    pass
