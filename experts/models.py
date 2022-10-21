7072fccc650eb9751a98aac86b3ac99b48ee21f6
k-roozbahani <kavehrozbah@gmail.com>
2022-10-21 18:54:49 +0330

    modified all models for MigrateError

--- experts/models.py
+++ experts/models.py
@@ -1,12 +1,12 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 
 class Expert(models.Model):
     SITE_CHITGHAR = 1
     SITE_ZARAFSHAN = 2
     SITE_SEMNAN = 3
     SITES = ((SITE_SEMNAN, 'site Chitghar'), (SITE_ZARAFSHAN, 'site Zarafshan'), (SITE_SEMNAN, 'site Semnan'))
-    user = models.OneToOneField('User', models.CASCADE)
+    user = models.OneToOneField(User, models.CASCADE)
     personal_id = models.IntegerField(unique=True)
     date_employment = models.DateField("date employment", null=True, blank=True)
     site = models.SmallIntegerField('site', default=1, choices=SITES)
--- shifts/models.py
+++ shifts/models.py
@@ -8,11 +8,14 @@ class ProgramMonth(models.Model):
     SITE_ZARAFSHAN = 2
     SITE_SEMNAN = 3
     SITES = ((SITE_SEMNAN, 'Site Chitghar'), (SITE_ZARAFSHAN, 'Site Zarafshan'), (SITE_SEMNAN, 'Site Semnan'))
-    name = models.CharField(max_length=30)
+    name = models.CharField(max_length=50)
     start_date = jmodels.jDateField()
     finish_date = jmodels.jDateField()
     site = models.PositiveSmallIntegerField(choices=SITES, default=1)
-    number_of_days = models.PositiveSmallIntegerField(max_length=31)
+    number_of_days = models.PositiveSmallIntegerField(
+
+
+    )
     number_of_holidays = models.PositiveSmallIntegerField()
     duty_time = models.PositiveIntegerField()
 
@@ -33,7 +36,7 @@ class ExpertShift(models.Model):
     STATUS_ENABLE = 2
     STATUS_PASS = 3
     STATUS_CHANGE = 4
-    STATUS = ((STATUS_RESERVE, 'Reserve'), (STATUS_ENABLE, 'Enable'), (STATUS_PASS, 'Pass'), (S))
+    STATUS = ((STATUS_RESERVE, 'Reserve'), (STATUS_ENABLE, 'Enable'), (STATUS_PASS, 'Pass'), (STATUS_CHANGE, 'Change'))
     experts = models.ForeignKey(Expert, models.PROTECT)
     shift = models.ForeignKey(Shift, models.CASCADE)
     status = models.PositiveSmallIntegerField(default=1, choices=STATUS)
